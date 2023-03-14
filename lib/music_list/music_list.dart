358b87ac2bb2241fce79d6413b8f1f8f3d38482b
Harikrishnan-cr <106924175+Harikrishnan-cr@users.noreply.github.com>
2023-03-14 06:21:23 +0530

    custom scrool

--- lib/music_list/music_list.dart
+++ lib/music_list/music_list.dart
@@ -69,3 +69,138 @@ return ListTile(
     },);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+class SampleApp extends StatefulWidget {
+  const SampleApp({Key? key}) : super(key: key);
+
+  @override
+  _SampleAppState createState() => _SampleAppState();
+}
+
+class _SampleAppState extends State<SampleApp> {
+  double appBarHeight = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text('Unfixed App Bar'),
+            //floating: true,
+            //snap: true,
+          ),
+
+SliverToBoxAdapter( 
+    child: Container(
+      width: double.infinity,
+      height: 380,
+      color: Colors.amber,
+    ),
+),
+          // SliverList(
+          //   delegate: SliverChildBuilderDelegate(
+          //     (BuildContext context, int index) {
+          //       return Container(width: double.infinity,height: 380,color: Colors.amber,);
+          //     }, 
+          //     childCount: 1,
+          //   ),
+          // ),
+SliverToBoxAdapter( 
+    child: SizedBox(
+      height: 30,
+    ),
+),
+          SliverList(  
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text('$index'),
+                  ),
+                  title: Text('List Item $index'),
+                  subtitle: Text('This is a subtitle'),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {},
+                );
+              },
+              childCount: 50,
+            ),
+          ),
+        ],
+      ),
+    );
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Scaffold(
+    //   body: CustomScrollView(
+    //     slivers: <Widget>[
+    //       SliverAppBar(
+    //         expandedHeight: appBarHeight,
+    //         flexibleSpace: FlexibleSpaceBar(
+    //           title: Text('Movable App Bar'),
+    //         ),
+    //         pinned: true,
+    //         backgroundColor: Colors.blue,
+    //       ),
+    //       SliverList(
+    //         delegate: SliverChildBuilderDelegate(
+    //           (BuildContext context, int index) {
+    //             return ListTile(
+    //               leading: CircleAvatar(
+    //                 child: Text('$index'),
+    //               ),
+    //               title: Text('List Item $index'),
+    //               subtitle: Text('This is a subtitle'),
+    //               trailing: Icon(Icons.arrow_forward),
+    //               onTap: () {},
+    //             );
+    //           },
+    //           childCount: 50,
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    //   bottomNavigationBar: BottomAppBar(
+    //     child: Row(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         IconButton(
+    //           icon: Icon(Icons.arrow_drop_down),
+    //           onPressed: () {
+    //             setState(() {
+    //               appBarHeight -= 50;
+    //             });
+    //           },
+    //         ),
+    //         IconButton(
+    //           icon: Icon(Icons.arrow_drop_up),
+    //           onPressed: () {
+    //             setState(() {
+    //               appBarHeight += 50;
+    //             });
+    //           },
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+}
